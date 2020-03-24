386d3532282c21e50c94a1a73c8221f9ebe1443b
sophie <sneuville59@gmail.com>
2020-03-24 18:09:12 +0100

    resolu

--- exercises/total.js
+++ exercises/total.js
@@ -7,7 +7,13 @@
  */
 
 // Your code:
-
+function total (arr){
+    let sum = 0;
+   for(let i = 0; i < arr.length; i++) {
+        sum += arr[i]}
+        return sum
+   
+}
 //* Begin of tests
 const assert = require('assert');
 
